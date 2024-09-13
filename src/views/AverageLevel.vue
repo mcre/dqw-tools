9637383dc1c54be91093670ba7b433583eb0af52
mcre <mc@mchs-u.net>
2024-09-13 12:14:07 +0900

    特級職レベル上限75に対応

--- src/views/AverageLevel.vue
+++ src/views/AverageLevel.vue
@@ -68,7 +68,7 @@
             track-color="default"
             color="primary"
             min="1"
-            max="115"
+            max="120"
             v-model="averageLevel"
             readonly
             thumb-label="always"
@@ -114,7 +114,7 @@ const maxLevel = (type: LevelType): number => {
     case "advanced":
       return 90;
     case "special":
-      return 70;
+      return 75;
     default:
       return 55;
   }
